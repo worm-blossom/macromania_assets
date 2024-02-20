595f653c634267e90eb9f2c852380426203d0763
Aljoscha Meyer <mail@aljoscha-meyer.de>
2024-02-20 21:07:37 +0100

    Oops, I can't git

---
+++ deps.ts
@@ -0,0 +1,6 @@
+export * from "../macromania/mod.ts";
+export * from "../macromania-logger/mod.tsx";
+export * from "../macromania-outfs/mod.tsx";
+export * from "../macromania-fs/mod.tsx";
+export * as path from "https://deno.land/std@0.216.0/path/mod.ts";
+export * as posixPath from "https://deno.land/std@0.216.0/path/posix/mod.ts";
---
+++ devDeps.ts
@@ -0,0 +1,2 @@
+export { assertEquals } from "https://deno.land/std@0.213.0/assert/mod.ts";
+export { assertFs } from "../assert-files/mod.ts";
